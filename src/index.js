8a5a1b311c05f536e751fda6af27ee0867e1c9ad
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-11-27 21:30:26 +0530

    added createTwitchTV function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-const url = "https://wind-bow.glitch.me/twitch-api/users/freecodecamp";
-const users = [
-  "ESL_SC2",
-  "OgamingSC2",
-  "cretetion",
-  "freecodecamp",
-  "storbeck",
-  "habathcx",
-  "RobotCaleb",
-  "noobs2ninjas"
-];
+function createTwitchTV() {
+  const url = "https://wind-bow.glitch.me/twitch-api/users/";
+  const users = [
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "freecodecamp",
+    "storbeck",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas"
+  ];
+  const usersList = document.querySelector(".users-list");
+  const twitchTV = {
+    createElement: element => document.createElement(element),
+    appendElement: (parent, child) => parent.appendElement(child),
+    populateUsers: () => {
+      users.map(user => {
+        fetch(`${url}user`)
+          .then(data => data.json())
+          .then(data => console.log(data));
+      });
 
-function createElement(element) {
-  return document.createElement(ul);
+      const ulItem = twitchTV.createElement(ul);
+      const liImgItem = twitchTV.createElement(li);
+      const imgItem = twitchTV.createElement(img);
+      const liUserNameItem = twitchTV.createElement(ul);
+      twitchTV.appendElement(usersList, ulItem);
+      twitchTV.appendElement(ulItem, liImgItem);
+      twitchTV.appendElement(liImgItem, imgItem);
+      twitchTV.appendElement(ulItem, liUserNameItem);
+    }
+  };
 }
 
-function appendElement(parent, child) {}
-
-fetch(url)
-  .then(data => data.json())
-  .then(data => console.log(data));
+// fetch(`${url}freecodecamp`)
+//   .then(data => data.json())
+//   .then(data => console.log(data));
