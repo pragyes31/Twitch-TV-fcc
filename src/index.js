84d75947f1157daedba9abe8d9e4b28bed955cfd
Rahul Nayak <pragyes31@users.noreply.github.com>
2018-11-28 16:56:48 +0530

    users populating

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
-function createTwitchTV() {
-  const url = "https://wind-bow.glitch.me/twitch-api/users/";
-  const users = [
-    "ESL_SC2",
-    "OgamingSC2",
-    "cretetion",
-    "freecodecamp",
-    "storbeck",
-    "habathcx",
-    "RobotCaleb",
-    "noobs2ninjas"
-  ];
-  const usersList = document.querySelector(".users-list");
-  const twitchTV = {
-    createElement: element => document.createElement(element),
-    appendElement: (parent, child) => parent.appendElement(child),
-    populateUsers: () => {
-      users.map(user => {
-        fetch(`${url}user`)
-          .then(data => data.json())
-          .then(data => console.log(data));
-      });
+const url = "https://wind-bow.glitch.me/twitch-api/users/";
+const users = [
+  "ESL_SC2",
+  "OgamingSC2",
+  "cretetion",
+  "freecodecamp",
+  "storbeck",
+  "habathcx",
+  "RobotCaleb",
+  "noobs2ninjas"
+];
+const usersList = document.querySelector(".users-list");
 
-      const ulItem = twitchTV.createElement(ul);
-      const liImgItem = twitchTV.createElement(li);
-      const imgItem = twitchTV.createElement(img);
-      const liUserNameItem = twitchTV.createElement(ul);
-      twitchTV.appendElement(usersList, ulItem);
-      twitchTV.appendElement(ulItem, liImgItem);
-      twitchTV.appendElement(liImgItem, imgItem);
-      twitchTV.appendElement(ulItem, liUserNameItem);
-    }
-  };
-}
+const twitchTV = {
+  populateUsers: () => {
+    console.log("123");
+    users.map(user => {
+      fetch(`${url}${user}`)
+        .then(data => data.json())
+        .then(data => twitchTV.populateUser(data));
+    });
+  },
+  populateUser: data => {
+    const ulItem = document.createElement("ul");
+    ulItem.classList.add = "user-item";
+    const liImgItem = document.createElement("li");
+    const imgItem = document.createElement("img");
+    const liUserNameItem = document.createElement("li");
+    usersList.appendChild(ulItem);
+    ulItem.appendChild(liImgItem);
+    liImgItem.appendChild(imgItem);
+    ulItem.appendChild(liUserNameItem);
+    imgItem.src = data.logo;
+    liUserNameItem.innerHTML = data.name;
+  }
+};
+twitchTV.populateUsers();
 
 // fetch(`${url}freecodecamp`)
 //   .then(data => data.json())
